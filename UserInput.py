3bf7c1012a57a21be6bb209571aabd6ca55e0f81
wilfridasisilia <wilfridasisilia05@gmail.com>
2024-07-21 13:07:12 +0700

    User Input -Bagian3

---
+++ UserInput.py
@@ -0,0 +1,10 @@
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+number = int(input("Input any number?: "))
+height = float(input("How tall are youu?: "))
+
+print("Your name is " + name)
+print("You are " + age + " yo")
+number += 1
+print("Your number plus 1 would be " + str(number))
+print("You " + str(height) + " cm")
