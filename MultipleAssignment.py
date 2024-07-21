f447bdd8249b184c73de1489f04bc3ef3beea46b
wilfridasisilia <wilfridasisilia05@gmail.com>
2024-07-21 12:48:51 +0700

    MultipleAssignment and Type Casting -Bagian2

---
+++ MultipleAssignment.py
@@ -0,0 +1,37 @@
+# Multiple Assignment = allows us to assign variable at the same time in one
+# line of code
+from http.cookiejar import uppercase_escaped_char
+
+# nama = "Frida"
+# age = 18
+# there = True
+
+# name, age, attractive = "Frida sisilia", 19, True
+# # Melihat Panjang Data
+# print(len(name))
+# print(name.find("a"))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.count("l"))
+# print(name.replace("a," + "o"))
+# print int(name*3)
+
+#=======================================================================================
+
+# Type Casting = Convert the data type of a value to anothe data type
+x = 1 #int
+y = 2.0 #float
+z = "3" #str
+print(x)
+print(y)
+print(z)
+
+y = int(y)
+z = int(z)
+# print(type(y))
+print(z*3)
+
+# ========================================================================================
+
