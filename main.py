c5e157f4c3226b860c65bb83b6d5940ea0d7cdc0
wilfridasisilia <wilfridasisilia05@gmail.com>
2024-07-21 12:06:21 +0700

    Variable -Bagian1 Update

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
-print("I love Pizza")
-print("It's Really Good!")
+# print("I love Pizza")
+# print("It's Really Good!")
+
+# Start
+# VARIABLE = Is a container for a value. Bahaves as the value that it contains.
+# Tipe Data 1 String
+first_name = "Frida"
+last_name = "Sisilia"
+full_name = first_name + " " + last_name;
+# print(type(name))
+print("Hello " + full_name)
+
+# Tipe Data 2 Integer
+age = 18
+age += 1
+# print(type(age))
+print(age)
+print(full_name + " Age is " + str(age))
+
+# Tipe Data 3 Float (Floating Point Number)
+height = 250.5
+print(str(height))
+# print(type(height))
+
+# Tipe Data 4 Boolean
+human = True
+# print(type(human))
+print("Are you a human? : " + str(human))
